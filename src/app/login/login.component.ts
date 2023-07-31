15294b0fe9c1e5be676c54fea939d17a609e3114
yasslne <mdyassinechiboub@gmail.com>
2023-07-31 11:26:20 +0100

    login page with tailwindcss

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,7 +5,7 @@ import { StorageService } from '../services/storage.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  // styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
   form: any = {
